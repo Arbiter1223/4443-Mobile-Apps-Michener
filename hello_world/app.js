9c84e8e4238757ba06b7e4fcb02a2223d1c09044
Arbiter1223 <Arbiter1223@live.com>
2018-10-08 16:41:48 -0500

    +Added repository to GHDesktop
    +Added repository to GitHubDesktop
    +Pushed current work to GitHub

---
+++ hello_world/app.js
@@ -0,0 +1,14 @@
+var msg = 'Hello World';
+console.log(msg);
+
+//get Android Studio
+//latest, and maybe lolipop
+//get an emulator
+//gradle
+//install ionic
+
+// npm install -g @angular/cli@latest
+// ng new fireshareProj --skip--install
+// npm install
+
+//ng (up-arrow stuff)
